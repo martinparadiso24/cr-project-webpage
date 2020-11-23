8f47f044d304c1f678b6455dc34a25e8704bd718
Martin <martin@Martins-MacBook-Pro.local>
2020-11-22 22:02:41 -0300

    primer pagina

---
+++ js/accordion.js
@@ -0,0 +1,26 @@
+// JS de Accordion sacado de W3Schools
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// JQuery Delay accordion
+
+$(document).ready(function() {
+  $('#accordionContainer').delay(1000).fadeIn(1000);
+});
